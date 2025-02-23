f83ee68964d90058344795936029b0d6e1c30210
PengZai <649365461@qq.com>
2025-02-23 23:52:33 +0000

    optimize the coordinate of evaluation

--- ov_msckf/evaluation_results/BotanicGarden/batch_process.py
+++ ov_msckf/evaluation_results/BotanicGarden/batch_process.py
@@ -5,17 +5,26 @@ from evo.core.trajectory import PoseTrajectory3D
 import shutil
 
 # Define the input and output filenames
-folder = '1005_07'
-filename_prefix = 'openvins_stereo_inertial_1005_07_img10hz600p'
+sequence_name = '1018_00'
+sensor_type = 'stereo_inertial'
+filename_prefix = '_'.join(['openvins', sensor_type, 'for', sequence_name, 'img10hz600p'])
 openvins_estimated_surffix = 'traj_estimate'
 openvins_timing_surffix = 'traj_timing'
 
+if not os.path.exists(os.path.join(sequence_name, filename_prefix)):
+    os.makedirs(os.path.join(sequence_name, filename_prefix))
 
-if not os.path.exists(os.path.join(folder, filename_prefix)):
-    os.makedirs(os.path.join(folder, filename_prefix))
+testfolder_names = [f for f in os.listdir(os.path.join(sequence_name, filename_prefix))]
+current_testfolder_name = '_'.join(['test', str(len(testfolder_names))])
+if not os.path.exists(os.path.join(sequence_name, filename_prefix, current_testfolder_name)):
+    os.makedirs(os.path.join(sequence_name, filename_prefix, current_testfolder_name))
+if not os.path.exists(os.path.join(sequence_name, filename_prefix, current_testfolder_name, 'config')):
+    os.makedirs(os.path.join(sequence_name, filename_prefix, current_testfolder_name, 'config'))    
+
+
 
 # Open the input file for reading and the output file for writing
-with open(os.path.join(folder, '_'.join([filename_prefix, openvins_estimated_surffix+'.txt'])), 'r') as infile, open(os.path.join(folder, filename_prefix, '_'.join([filename_prefix, openvins_estimated_surffix, 'tum_format.txt'])), 'w') as outfile:
+with open(os.path.join(sequence_name, '_'.join([filename_prefix, openvins_estimated_surffix+'.txt'])), 'r') as infile, open(os.path.join(sequence_name, filename_prefix, current_testfolder_name, '_'.join([filename_prefix, openvins_estimated_surffix, 'tum_format.txt'])), 'w') as outfile:
     # Process each line in the input file
     for line in infile:
         # Remove leading/trailing whitespace
@@ -32,18 +41,19 @@ with open(os.path.join(folder, '_'.join([filename_prefix, openvins_estimated_sur
         # Write the new line to the output file
         outfile.write(new_line + '\n')
 
-shutil.copy(os.path.join(folder, '_'.join([filename_prefix, openvins_estimated_surffix+'.txt'])), os.path.join(folder, filename_prefix, '_'.join([filename_prefix, openvins_estimated_surffix+'.txt'])))
-shutil.copy(os.path.join(folder, '_'.join([filename_prefix, openvins_timing_surffix+'.txt'])), os.path.join(folder, filename_prefix, '_'.join([filename_prefix, openvins_timing_surffix+'.txt'])))
+shutil.copy(os.path.join(sequence_name, '_'.join([filename_prefix, openvins_estimated_surffix+".txt"])), os.path.join(sequence_name, filename_prefix, current_testfolder_name, '_'.join([filename_prefix, openvins_estimated_surffix+'.txt'])))
+shutil.copy(os.path.join(sequence_name, '_'.join([filename_prefix, openvins_timing_surffix+".txt"])), os.path.join(sequence_name, filename_prefix, current_testfolder_name, '_'.join([filename_prefix, openvins_timing_surffix+'.txt'])))
+shutil.copytree(os.path.join('../../../config', 'BotanicGarden'), os.path.join(sequence_name, filename_prefix, current_testfolder_name, 'config', 'BotanicGarden'))
 
 
 
 # because KeyFrame is in Xsens coordinates, so we need to transfer it to RGB0 coordinates
 # RGB0 in Xsens coordinates
 sensor_coordinate_transform_matrix_for_camera_frame = np.array([
-    [1., 0.,   0.,  0],  
-    [0., 1.,   0.,  0],  
-    [0., 0.,   1.,  0],  
-    [0., 0,    0,   1.0]
+    [0.999678872580465,0.0252865664429322,0.00150422292234868,0.0584867781527745],  
+    [-0.0252723438960774,0.999649431893338,-0.0078025434141585,0.00840419966766332],  
+    [-0.00170103929405540,0.00776298237926191,0.99996789371916,0.168915521980526],  
+    [0.0,0.0,0.0,1.0]
 ])
 
 # seems x in key frame of orbslam system is -z in BotanicGarden
@@ -54,13 +64,47 @@ system_coordinate_transform_matrix_for_camera_frame = np.array([
     [0., 0,    0,   1.0]
 ])
 
-traj = file_interface.read_tum_trajectory_file(open(os.path.join(folder, filename_prefix, '_'.join([filename_prefix, openvins_estimated_surffix, 'tum_format.txt']))))
+# system_coordinate_transform_matrix_for_camera_frame = np.array([
+#     [1., 0.,   0.,  0],  
+#     [0., 1.,   0.,  0],  
+#     [0., 0.,   1.,  0],  
+#     [0., 0,    0,   1.0]
+# ])
+
+def invert_transformation_matrix(T):
+    """
+    Computes the inverse of a 4x4 homogeneous transformation matrix.
+    
+    Parameters:
+        T (numpy.ndarray): 4x4 transformation matrix
+    
+    Returns:
+        numpy.ndarray: 4x4 inverse transformation matrix
+    """
+    # Extract rotation (R) and translation (t)
+    R = T[:3, :3]  # 3x3 rotation matrix
+    t = T[:3, 3]   # 3x1 translation vector
+
+    # Compute the inverse transformation
+    R_inv = R.T  # Transpose of rotation matrix
+    t_inv = -R_inv @ t  # Compute new translation
+
+    # Construct the inverse transformation matrix
+    T_inv = np.eye(4)
+    T_inv[:3, :3] = R_inv
+    T_inv[:3, 3] = t_inv
+
+    return T_inv
+
+
+
+traj = file_interface.read_tum_trajectory_file(open(os.path.join(sequence_name, filename_prefix, current_testfolder_name, '_'.join([filename_prefix, openvins_estimated_surffix, 'tum_format.txt']))))
 
 transformed_poses = []
 for pose in traj.poses_se3:
 
-    
-    transformed_pose_matrix =   system_coordinate_transform_matrix_for_camera_frame @ sensor_coordinate_transform_matrix_for_camera_frame @ pose
+    # Tvi * Tib
+    transformed_pose_matrix = system_coordinate_transform_matrix_for_camera_frame @ invert_transformation_matrix(sensor_coordinate_transform_matrix_for_camera_frame) @ pose
 
     # Append transformed pose as SE3 object
     transformed_poses.append(transformed_pose_matrix)
@@ -70,4 +114,4 @@ traj = PoseTrajectory3D(
     timestamps=traj.timestamps
 )
 
-file_interface.write_tum_trajectory_file(os.path.join(folder, filename_prefix, '_'.join([filename_prefix, openvins_estimated_surffix, 'coordinate_aligned.txt'])), traj)
+file_interface.write_tum_trajectory_file(os.path.join(sequence_name, filename_prefix, current_testfolder_name, '_'.join([filename_prefix, openvins_estimated_surffix, 'coordinate_aligned.txt'])), traj)
--- ov_msckf/evaluation_results/evo_eval.sh
+++ ov_msckf/evaluation_results/evo_eval.sh
@@ -1,14 +1,14 @@
 
 DATASET="BotanicGarden"
-DIR="1005_07"
-
-ESITMATED_FOLDER="openvins_stereo_inertial_${DIR}_img10hz600p"
+DIR="1018_00"
+TEST_DIR="test_0"
+ESITMATED_FOLDER="openvins_monocular_inertial_for_${DIR}_img10hz600p"
 REFERENCE="${DIR}_GT_output.txt"
 
-evo_rpe tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/rpe_${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.log
-evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/ape_${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.log
-evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE --align --pose_relation trans_part > $DATASET/$DIR/${ESITMATED_FOLDER}/ate_${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.log
+evo_rpe tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/rpe_${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.log
+evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE  > $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/ape_${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.log
+evo_ape tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt $DATASET/$DIR/$REFERENCE --align --pose_relation trans_part > $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/ate_${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.log
 
 
-evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xyz --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/3d  
-evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xy --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/2d 
+evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xyz --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/3d  
+evo_traj tum $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/${ESITMATED_FOLDER}_traj_estimate_coordinate_aligned.txt --ref=$DATASET/$DIR/$REFERENCE -p --plot_mode=xy --save_plot $DATASET/$DIR/${ESITMATED_FOLDER}/${TEST_DIR}/2d 
